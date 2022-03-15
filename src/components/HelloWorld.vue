eda7814a84a6518b399beeedc51c08589879f534
abdulrehman1363 <abdulrehmanaziz256@gmail.com>
2022-03-15 14:58:48 +0500

    some changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!-- Voice Record Title + Button -->
+  <div class="form-group row">
+    <div class="col-md-12 align-items-center">
+      <input type="file" @change="onFileChange">
+      <button @click="recordAudio()" type="button" id="button_record" class="btn btn-danger">
+        Record
+      </button>
+      <button type="button" id="button_stop" class="btn btn-success" @click="stop">
+        Stop
+      </button>
+      <div id="audio" class="audio">
+      </div>
+    </div>
+  </div>
+  <div class="row" style="margin-top: 10px">
+    <div class="col-md-12 align-items-center">
+      <button type="button" class="btn btn-outline-success" style="width: 436px;">
+        Upload
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      recorder: null,
+      items : [],
+      files: null
+    }
+  },
+  methods: {
+
+    recordAudio() {
+      let device = navigator.mediaDevices.getUserMedia({ audio: true });
+      device.then((stream) => {
+
+        this.recorder = new MediaRecorder(stream);
+        this.recorder.start();
+        this.recorder.ondataavailable = (e) => {
+          this.items.push(e.data);
+          if (this.recorder.state == "inactive") {
+            let blob = new Blob(this.items, { type: "audio/*" });
+            let audio = document.getElementById("audio");
+            let mainaudio = document.createElement("audio");
+            mainaudio.setAttribute("controls", "controls");
+            audio.appendChild(mainaudio);
+            mainaudio.src = URL.createObjectURL(blob);
+          }
+        };
+      });
+    },
+    // called on button click
+    stop() {
+      this.recorder.stop()
+    },
+    onFileChange(e){
+       this.files = e.target.files || e.dataTransfer.files;
+      if (!this.files.length) return;
+      console.log('file = ', this.files[0])
+    }
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
